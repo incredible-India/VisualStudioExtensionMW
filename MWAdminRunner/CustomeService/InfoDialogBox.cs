3528f00a9a737df0b44203bbf041f7baba957242
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-09-02 20:36:41 +0530

    Required Files Checking Functionality Done :)

--- MWAdminRunner/CustomeService/InfoDialogBox.cs
+++ MWAdminRunner/CustomeService/InfoDialogBox.cs
@@ -18,6 +18,8 @@ namespace MWAdminRunner.CustomeService
         private readonly string middleWareSolutionFileName = "incadea.api.middleware";
         private readonly uint maxProjectInsideProject = 100;
         private readonly string[] RequiredProjects = new string[] { "incadea.api.middleware.admin", "incadea.api.middleware.web" };
+        private readonly string[] RequiredFilesAdmin = new string[] { "appsettings.json","Program.cs", "Properties/launchSettings.json" };
+        private readonly string[] RequiredFilesWeb = new string[] { "appsettings.json" };
 
         public InfoDialogBox(IVsSolution vsSolution)
         {
@@ -62,16 +64,58 @@ namespace MWAdminRunner.CustomeService
                                        out pcProjectsFetched
 
                                 );
-
+            //"C:\\MW\\Datastream\\WAP-API Components\\incadea.API Middleware\\incadea.api.middleware.admin\\incadea.api.middleware.admin.csproj\\appsettings.json"
             //removing null values
             string[] allProjectsInsideSolutionWithPath = cProjects.Where(i => i != null).ToArray();
             if(allProjectsInsideSolutionWithPath.Length > 0)
+
             {
+               // "C:/MW/Datastream/WAP-API Components/incadea.API Middleware/incadea.api.middleware.admin/appsettings.json"
+                //"C:\\MW\\Datastream\\WAP-API Components\\incadea.API Middleware\\incadea.api.middleware.admin\\appsettings.json"
+                //C:\MW\CoreLogs\WAP-API Components\incadea.API Middleware\incadea.api.middleware.admin
+                string[] allProjectInsideSolutionOnlyPath = allProjectsInsideSolutionWithPath.Select(i=>Path.GetDirectoryName(i)).ToArray();//damiler will get skipped
                 string[] allProjectInsideSolutionWithoutPath = allProjectsInsideSolutionWithPath.Select(y => Path.GetFileNameWithoutExtension(y)).ToArray();
                 bool allPresent = this.RequiredProjects.All(element => allProjectInsideSolutionWithoutPath.Contains(element));
-                if (allPresent) {
-                    return true;
+                if (allPresent)
+                {
+                    int CountForAdmin=0;
+                    int CountForWeb = 0;
+                    for (int i = 0; i< allProjectInsideSolutionOnlyPath.Length;i++)
+                    {
+                        //admin
+                        if (allProjectInsideSolutionOnlyPath[i].Contains(this.RequiredProjects[0]))
+                        {
+                            for (int j = 0; j < this.RequiredFilesAdmin.Length; j++)
+                            {
+                                var FilePathToCheckInsideProject = (allProjectInsideSolutionOnlyPath[i] + $"\\{this.RequiredFilesAdmin[0]}").Replace('\\', '/');
+                                var isThere = File.Exists(FilePathToCheckInsideProject); 
+                                if(isThere)
+                                    CountForAdmin++;
+                            }
+                           
+                        }
+                        //web
+                        if (allProjectInsideSolutionOnlyPath[i].Contains(this.RequiredProjects[1]))
+                        {
+                            for (int j = 0; j < this.RequiredFilesWeb.Length; j++)
+                            {
+                                var FilePathToCheckInsideProjectWeb = (allProjectInsideSolutionOnlyPath[i] + $"\\{this.RequiredFilesAdmin[0]}").Replace('\\', '/');
+                                var isThereWeb = File.Exists(FilePathToCheckInsideProjectWeb);
+                                if(isThereWeb)
+                                    CountForWeb++;
+                            }
+
+                        }
+                    }
+                    if(CountForWeb == this.RequiredFilesWeb.Length && CountForAdmin== this.RequiredFilesAdmin.Length)
+                            return true;
+                    else
+                    {
+                        VS.MessageBox.ShowError("File Not Found", "Oh! Some Required Files need to be modified are not found!!! [appsettings.json(Admin/Web),Programe.cs(Admin) ,launchsettings.json (Admin)]");
+                        return false;
+                    }
                 }
+
                 else
                 {
                     VS.MessageBox.ShowError("Yaar!!", $"No Required Project Found inside your {GetSolutionName}.sln to modify, Please Check Once");
